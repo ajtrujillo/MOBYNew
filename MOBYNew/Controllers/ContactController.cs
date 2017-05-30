942538b17d8f3c74a7c7e46aab6f21ca22bd65f1
A.J. Trujillo <ajtrjll@gmail.com>
2017-05-30 16:36:15 -0500

    Initial Commit

---
+++ MOBYNew/Controllers/ContactController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MOBYNew.Models;
+using MOBYNew.ViewModels;
+
+namespace MOBYNew.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Contact
+        public ActionResult Index()
+        {
+            var contacts = new List<Contact>
+            {
+                new Contact() {Name = "Contact 1"},
+                new Contact() {Name = "Contact 2"}
+            };
+
+            var contactViewModel = new ContactViewModel();
+
+            return View(contactViewModel);
+        }
+    }
+}
---
+++ MOBYNew/ViewModels/ContactViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MOBYNew.Models;
+
+namespace MOBYNew.ViewModels
+{
+    public class ContactViewModel
+    {
+        public List<Contact> Contacts { get; set; }
+
+    }
+}
