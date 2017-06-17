6fa2d44a2b5effbe3c932989604ee4db6d96ee09
A.J. Trujillo <ajtrjll@gmail.com>
2017-06-17 13:00:20 -0500

    Finally got the Contacts link to go somewhere. Contacts list not displaying.

--- MOBYNew/Controllers/ContactController.cs
+++ MOBYNew/Controllers/ContactController.cs
@@ -13,15 +13,32 @@ namespace MOBYNew.Controllers
         // GET: Contact
         public ActionResult Index()
         {
-            var contacts = new List<Contact>
+            ContactViewModel contactList = new ContactViewModel();
+
             {
-                new Contact() {Name = "Contact 1"},
-                new Contact() {Name = "Contact 2"}
-            };
+                new Contact { Name = "Amy", Id = 1 };
+                new Contact { Name = "Ben", Id = 2 };
+                //List<Contact> contactsList = new List<Contact>
+                //{
+                //    new Contact {Name = "Amy", Id = 1},
+                //    new Contact {Name = "Dan", Id = 2},
 
-            var contactViewModel = new ContactViewModel();
+                //};
 
-            return View(contactViewModel);
+                //return View(contactsList);
+
+                //var contacts = new List<Contact>
+                //{
+                //    new Contact() {Name = "Contact 1", Id=1},
+                //    new Contact() {Name = "Contact 2", Id=2}
+                //};
+
+                //var contactViewModel = new ContactViewModel();
+
+                //return View(contactViewModel);
+            }
+
+            return View(contactList);
         }
     }
 }
