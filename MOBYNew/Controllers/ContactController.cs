c68297be009080cb3df092ba316809415f218b42
A.J. Trujillo <ajtrjll@gmail.com>
2017-06-26 16:15:50 -0500

    Disabled Owin Auto Startup in web.config>appSettings

--- MOBYNew/Controllers/ContactController.cs
+++ MOBYNew/Controllers/ContactController.cs
@@ -1,26 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 using System.Web.Mvc;
 using MOBYNew.Models;
-using MOBYNew.ViewModels;
 
 namespace MOBYNew.Controllers
 {
     public class ContactController : Controller
     {
-        // GET: Contact
-        public ActionResult Index()
-        {
-            List<Contact> contactList = new List<Contact>
+       private List<Contact> contactList = new List<Contact>
             {
                 new Contact {Name = "AJ", Id = 1},
                 new Contact {Name = "Ben", Id = 2},
                 new Contact {Name = "Jackson", Id = 3}
             };
 
+        // GET: Contact
+        public ActionResult Index()
+        {
             return View(contactList);
+        }
+
+        public ActionResult ContactDetail(int id)
+        {
+            Contact tempContact = new Contact { Id = id };
+            return View(tempContact);
         }
     }
 }
--- MOBYNew/Models/Contact.cs
+++ MOBYNew/Models/Contact.cs
@@ -1,9 +1,15 @@
-﻿namespace MOBYNew.Models
+﻿using System;
+
+namespace MOBYNew.Models
 {
     public class Contact
     {
         public int Id { get; set; }
         public string Name { get; set; }
 
+        internal static Contact FirstOrDefault(Func<object, bool> p)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
