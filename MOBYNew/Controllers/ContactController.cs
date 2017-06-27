111f405c7b408eccee9e62e1d53ae7111c571761
A.J. Trujillo <ajtrjll@gmail.com>
2017-06-26 19:24:20 -0500

    Added "No contacts yet" exception to contact controller

--- MOBYNew/Controllers/ContactController.cs
+++ MOBYNew/Controllers/ContactController.cs
@@ -6,7 +6,7 @@ namespace MOBYNew.Controllers
 {
     public class ContactController : Controller
     {
-       private List<Contact> contactList = new List<Contact>
+        private List<Contact> contactList = new List<Contact>
             {
                 new Contact {Name = "AJ", Id = 1},
                 new Contact {Name = "Ben", Id = 2},
@@ -15,15 +15,25 @@ namespace MOBYNew.Controllers
 
         // GET: Contact
         public ActionResult Index()
+
         {
-            return View(contactList);
+            if (contactList.Count == 0)
+
+            { return Content("No contacts yet"); }
+
+            else
+
+            { return View(contactList); }
         }
 
         //GET: Contact Detail view
         public ActionResult ContactDetail(int id)
         {
             Contact contactDetail = new Contact { Id = id };
-            return View(contactDetail);
+
+            {
+                return View(contactDetail);
+            }
         }
     }
 }
