96b3e18839e0e3e0f68fb58659bc9d47ce5b8200
A.J. Trujillo <ajtrjll@gmail.com>
2017-07-14 11:42:10 -0500

    Added ContactType

---
+++ MOBYNew/Models/ContactStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MOBYNew.Models
+{
+    public class ContactStatus
+    {
+        public byte Id { get; set; }
+        public string contactStatus { get; set; }
+    }
+}
