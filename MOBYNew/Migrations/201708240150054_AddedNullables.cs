bec7732f163a1325f4bff8e9adc3e302d30f1d8d
A.J. Trujillo <ajtrjll@gmail.com>
2017-08-23 18:32:17 -0500

    Final backup prior to changing targeted version of .net for project.

---
+++ MOBYNew/Migrations/201708240150054_AddedNullables.cs
@@ -0,0 +1,16 @@
+namespace MOBYNew.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddedNullables : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
