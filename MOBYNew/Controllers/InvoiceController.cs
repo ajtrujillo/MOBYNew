5b02fc8f9925880397c7dd36e99c02453af81db6
A.J. Trujillo <ajtrjll@gmail.com>
2017-09-27 21:36:47 -0500

    Totally redid Items, working like a charm!!

--- MOBYNew/Controllers/InvoiceController.cs
+++ MOBYNew/Controllers/InvoiceController.cs
@@ -14,19 +14,16 @@ namespace MOBYNew.Controllers
         {
             _context = new ApplicationDbContext();
 
-            Invoice invoice = new Models.Invoice();
+            //Invoice invoice = new Models.Invoice();
 
             var CategoryList = new List<string>();
-
-            //var CategoryQuery = from i in _context.Items
-            //                    orderby i.Category
-            //                    select i.Category;
 
             var CategoryQuery = from c in _context.Categories
                                 orderby c.CategoryName
                                 select c.CategoryName;
 
             CategoryList.AddRange(CategoryQuery.Distinct());
+
             ViewBag.itemCategory = new SelectList(CategoryList);
 
             var items = from m in _context.Items
--- MOBYNew/Controllers/ItemController.cs
+++ MOBYNew/Controllers/ItemController.cs
@@ -1,6 +1,6 @@
-﻿using System.Collections.Generic;
-using System.Data.Entity;
+﻿using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web.Mvc;
 using MOBYNew.Models;
 
@@ -8,51 +8,125 @@ namespace MOBYNew.Controllers
 {
     public class ItemController : Controller
     {
-        private ApplicationDbContext _context;
+        private ApplicationDbContext db = new ApplicationDbContext();
 
-        public ItemController()
+        // GET: ItemController
+        public ActionResult Index()
         {
-            _context = new ApplicationDbContext();
+            var items = db.Items.Include(i => i.Category).Include(i => i.Genre);
+            return View(items.ToList());
+        }
+
+        // GET: ItemController/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Item item = db.Items.Find(id);
+            if (item == null)
+            {
+                return HttpNotFound();
+            }
+            return View(item);
+        }
+
+        // GET: ItemController/Create
+        public ActionResult Create()
+        {
+            ViewBag.CategoryId = new SelectList(db.Categories, "Id", "CategoryName");
+            ViewBag.GenreId = new SelectList(db.Genres, "Id", "genreName");
+            return View();
+        }
+
+        // POST: ItemController/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,ItemName,ReleaseDate,GenreId,CategoryId,ItemDescription,ImagePath,Price,ISBN13EAN,QtyInStock")] Item item)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Items.Add(item);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CategoryId = new SelectList(db.Categories, "Id", "CategoryName", item.CategoryId);
+            ViewBag.GenreId = new SelectList(db.Genres, "Id", "genreName", item.GenreId);
+            return View(item);
+        }
+
+        // GET: ItemController/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Item item = db.Items.Find(id);
+            if (item == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CategoryId = new SelectList(db.Categories, "Id", "CategoryName", item.CategoryId);
+            ViewBag.GenreId = new SelectList(db.Genres, "Id", "genreName", item.GenreId);
+            return View(item);
+        }
+
+        // POST: ItemController/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,ItemName,ReleaseDate,GenreId,CategoryId,ItemDescription,ImagePath,Price,ISBN13EAN,QtyInStock")] Item item)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(item).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CategoryId = new SelectList(db.Categories, "Id", "CategoryName", item.CategoryId);
+            ViewBag.GenreId = new SelectList(db.Genres, "Id", "genreName", item.GenreId);
+            return View(item);
+        }
+
+        // GET: ItemController/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Item item = db.Items.Find(id);
+            if (item == null)
+            {
+                return HttpNotFound();
+            }
+            return View(item);
+        }
+
+        // POST: ItemController/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Item item = db.Items.Find(id);
+            db.Items.Remove(item);
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
 
         protected override void Dispose(bool disposing)
         {
-            _context.Dispose();
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
         }
-
-        //attribute route with constraints
-        //[Route("item/release/{year:regex(\\d{4})}/{month:regex(\\d{2}):range(1,12)}/{day:regex(\\d{2}):range(1,31)}")]
-        //public ActionResult ByReleaseDate(int year, int month, int? day)
-        //{
-        //    return Content(year + "/" + month + "/" + day);
-        //}
-
-        //// item
-        //public ActionResult Index(int? pageIndex, string sortBy)
-        //{
-        //    if (!pageIndex.HasValue) pageIndex = 1;
-
-        //    if (String.IsNullOrWhiteSpace(sortBy)) sortBy = "Name";
-
-        //    return Content(String.Format("pageIndex = {0}&sortBy={1}", pageIndex, sortBy));
-        //}     
-        public ViewResult Index()
-        {
-            var items = _context.Items.Include(i => i.Genre).ToList();
-
-            return View(items);
-
-        }
-
-        public ActionResult itemDetail(int id)
-        {
-            var item = _context.Items.Include(i => i.Genre).SingleOrDefault(i => i.Id == id);
-
-            if (item == null)
-                return HttpNotFound();
-
-            return View(item);
-        }
-
     }
 }
