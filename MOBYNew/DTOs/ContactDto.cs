b74afc3aea909853ff59da8b93db591034c0b626
A.J. Trujillo <ajtrjll@gmail.com>
2017-10-10 18:51:31 -0500

    Created a contact dto.

---
+++ MOBYNew/DTOs/ContactDto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using MOBYNew.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MOBYNew.DTOs
+{
+    public class ContactDto
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        public int ContactTypeId { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true)]
+        public DateTime? JoinDate { get; set; } = DateTime.Now;
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true)]
+        public DateTime? DOB { get; set; }
+
+        public bool IsSubscribedToNewsletter { get; set; }
+    }
+}
